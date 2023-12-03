07d5a5bc3622d46cd731f0cab3c5ee0c4dc27adc
k8sexpert <chandrasekharakula92@gmail.com>
2023-12-03 22:36:13 +0530

    terraform ccicd

--- Terraform/config.tf
+++ Terraform/config.tf
@@ -1,12 +1,12 @@
-provider "google" {
-  credentials = file("C:/Users/Lenovo/Downloads/chandu/terraform-gcp-poc/pub-sap-sbx-poc-406406-4de1dd040119.json")
-  project     = "pub-sap-sbx-poc-406406"
-  region      = "us-east4"
+# provider "google" {
+#   credentials = file("C:/Users/Lenovo/Downloads/chandu/terraform-gcp-poc/pub-sap-sbx-poc-406406-4de1dd040119.json")
+#   project     = "pub-sap-sbx-poc-406406"
+#   region      = "us-east4"
 
-  default_labels = {
-    environment = "poc"
-  }
-}
+#   default_labels = {
+#     environment = "poc"
+#   }
+# }
 
 terraform {
   backend "gcs" {
